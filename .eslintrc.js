13e16365ffb4e434e7f44c6f8633d9d695426e45
ryanuo <1577791638@qq.com>
2025-04-04 22:56:55 +0800

    chore(eslint): remove TypeScript parser configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,6 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
-  parser: "@typescript-eslint/parser",
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
